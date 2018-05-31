54a6fee388c28fe3134a84bd7305f546e60f9121
tuotuobukaixin <wuchao2@huawei.com>
2018-05-31 14:34:52 +0800

    update

--- src/jobtest/action/action.sh
+++ src/jobtest/action/action.sh
@@ -4,4 +4,4 @@ chmod 750 redis-cli
 chmod 750 jobtest
 nohup ./jobtest &
 sleep $timeout
-redis-cli -h $redis_url -p  $redis_port rpush joblist $jobname
+./redis-cli -h $redis_url -p  $redis_port rpush joblist $jobname
